c22a4c8c34883cece6c5e6b25c1af844d232ed2c
Roger V <roger.vanburgh@softwire.com>
2022-07-21 10:30:44 +0100

    exercise-1-Displayed items on index, added form to add new items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config
 
@@ -8,4 +9,13 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    itemsToDisplay = get_items()
+
+    return render_template('index.html', items=itemsToDisplay)
+
+@app.route('/add', methods=['POST'])
+def addNewItem():
+    newItemTitle = request.form['addItemFormTitle']
+    add_item(newItemTitle)
+    
+    return redirect('/')
