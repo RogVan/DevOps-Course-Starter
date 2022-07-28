762327e0fd9d30803f4b4e3d84ba319ec7c734d0
RogVan <82506463+RogVan@users.noreply.github.com>
2022-07-28 11:26:09 +0100

    exercise-1-Displayed items on index, added form to add new items (#1)
    * exercise-1-Displayed items on index, added form to add new items
    
    * exercise-1-Added completion / deletion functionality and sorted list items
    
    * exercise-1-Addressed review comments

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, delete_item, get_item, get_items, save_item
 
 from todo_app.flask_config import Config
 
@@ -8,4 +9,32 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    itemsToDisplay = sorted(get_items(), key=lambda item: item['status'], reverse=True)
+
+    return render_template('index.html', items=itemsToDisplay)
+
+@app.route('/add', methods=['POST'])
+def addNewItem():
+    newItemTitle = request.form['addItemFormTitle']
+
+    if newItemTitle == '':
+        return redirect('/')
+    
+    add_item(newItemTitle)
+
+    return redirect('/')
+
+@app.route('/mark-complete/<itemId>')
+def markItemComplete(itemId):
+    itemToUpdate = get_item(int(itemId))
+    itemToUpdate['status'] = 'Complete'
+    save_item(itemToUpdate)
+
+    return redirect('/')
+
+@app.route('/delete/<itemId>')
+def deleteItem(itemId):
+    delete_item(int(itemId))
+
+    return redirect('/')
+    
